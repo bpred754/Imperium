c21093468660ffd892807f61064b6af77ca57b71
Curtis Rose <ccr46@nau.edu>
2015-05-26 21:26:27 -0700

    Ship Movement

---
+++ Ship Movement/Assets/Scripts/Unit.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Unit : MonoBehaviour {
+
+	public bool selected = false;
+	private Color startColor = new Color(.2f,.2f,.2f);
+
+	// Update is called once per frame
+	private void Update () {
+		if (GetComponent<Renderer> ().isVisible && Input.GetMouseButton (0)) {
+			Vector3 camPos = Camera.main.WorldToScreenPoint(transform.position);
+			camPos.y = CameraOperator.InvertMouseY(camPos.y);
+			selected = CameraOperator.selection.Contains(camPos);
+		}
+		if (selected)
+			GetComponent<Renderer> ().material.color = Color.red;
+		else
+			GetComponent<Renderer> ().material.color = startColor;
+	}
+}
