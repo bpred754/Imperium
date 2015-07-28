264944d127af19e09b9f7ab02d652c4f249d6de4
Brian Redd <brianredd@gmail.com>
2015-07-27 22:03:29 -0700

    Fix bug - Fit players GUI to screen for different resolutions

--- Assets/Scripts/Player/GUIManager.cs
+++ Assets/Scripts/Player/GUIManager.cs
@@ -7,27 +7,45 @@ using System;
 
 public class GUIManager : MonoBehaviour
 {	
+	// Constants
+	private float GUI_WIDTH = 150f;
+	private float TAB_HEIGHT = 20f;
+	
 	private GameObject buildingList;
 	private Button buildingsTab;
 	private Canvas canvas;
 	private CameraManager cameraManager;
 	private Grid grid;
+	private float guiButtonContainerHeight;
 	private float guiScreenWidth = 0;
 	private float guiWorldWidth = 0;
 	private GameObject minimap;
 	private PersonPlayer player;
+	private float previousGuiButtonContainerHeight;
 	private GameObject tabs;
-	private RectTransform tabTransform;
 	private Button unitButton0;
 	private Button unitButton1;
 	private GameObject unitList;
 	private Button unitsTab;
+	private Text unitTabText;
 	
 	// Prefabs
 	private GameObject buildingListPrefab;
 	private GameObject minimapPrefab;
 	private GameObject tabsPrefab;
 	private GameObject unitListPrefab;
+
+	// Transforms
+	private RectTransform[] buildingButtonTransforms;
+	private RectTransform buildingListRectTransform;
+	private RectTransform buildingTabTransform;
+	private Text buildingTabText;
+	private RectTransform canvasRectTransform;
+	private RectTransform miniMapRectTransform;
+	private RectTransform tabTransform;
+	private RectTransform[] unitButtonTransforms;
+	private RectTransform unitListRectTransform;
+	private RectTransform unitTabTransform;
 
 	/*********************************************************************************/
 	/*	Functions inherited from MonoBehaviour	- Order: Relevance					 */		
@@ -42,47 +60,114 @@ public class GUIManager : MonoBehaviour
 		this.unitListPrefab = Resources.Load<GameObject> ("GUIUnitList");
 
 		GameObject playerGameObject = GetComponent<Transform> ().gameObject;
-		this.player = playerGameObject.GetComponent<PersonPlayer>();
+		this.player = playerGameObject.GetComponent<PersonPlayer> ();
 		this.cameraManager = playerGameObject.GetComponent<CameraManager> ();
 		
 		// Add Event System to scene
-		if (FindObjectOfType<EventSystem>() == null)
-		{
-			GameObject es = new GameObject("EventSystem", typeof(EventSystem));
-			es.AddComponent<StandaloneInputModule>();
+		if (FindObjectOfType<EventSystem> () == null) {
+			GameObject es = new GameObject ("EventSystem", typeof(EventSystem));
+			es.AddComponent<StandaloneInputModule> ();
 		}
 		
 		// Add canvas to scene
-		GameObject canvasObject = new GameObject("Canvas");
-		this.canvas = canvasObject.AddComponent<Canvas>();
+		GameObject canvasObject = new GameObject ("Canvas");
+		this.canvas = canvasObject.AddComponent<Canvas> ();
 		this.canvas.renderMode = RenderMode.ScreenSpaceOverlay;
 		canvasObject.AddComponent<GraphicRaycaster> ();
 		
 		// Add minimap to canvas
-		this.minimap = (GameObject)Instantiate(minimapPrefab);
-		this.minimap.transform.SetParent(canvas.transform, false);
+		this.minimap = (GameObject)Instantiate (minimapPrefab);
+		this.minimap.transform.SetParent (canvas.transform, false);
+		this.miniMapRectTransform = this.minimap.GetComponent<RectTransform> ();
 		
 		// Add tabs to canvas
-		this.tabs = (GameObject)Instantiate(tabsPrefab);
-		this.tabs.transform.SetParent(canvas.transform, false);
-		this.tabTransform = tabs.GetComponent<RectTransform>();
-		this.buildingsTab = GameObject.Find ("BuildingsTab").GetComponentInChildren<Button>();
-		this.unitsTab = GameObject.Find ("UnitsTab").GetComponentInChildren<Button>();
-		
+		this.tabs = (GameObject)Instantiate (tabsPrefab);
+		this.tabs.transform.SetParent (canvas.transform, false);
+		this.tabTransform = tabs.GetComponent<RectTransform> ();
+		this.buildingsTab = GameObject.Find ("BuildingsTab").GetComponentInChildren<Button> ();
+		this.buildingTabTransform = buildingsTab.GetComponent<RectTransform> ();
+		this.buildingTabText = buildingsTab.GetComponentInChildren<Text> ();
+
+		this.unitsTab = GameObject.Find ("UnitsTab").GetComponentInChildren<Button> ();
+		this.unitTabTransform = unitsTab.GetComponent<RectTransform> ();
+		this.unitTabText = unitsTab.GetComponentInChildren<Text> ();
+
 		// Add buildingList to prefab
-		this.buildingList = (GameObject)Instantiate(buildingListPrefab);
-		this.buildingList.transform.SetParent(canvas.transform, false);
-		
+		this.buildingList = (GameObject)Instantiate (buildingListPrefab);
+		this.buildingList.transform.SetParent (canvas.transform, false);
+		Button[] buildingListButtons = buildingList.GetComponentsInChildren<Button> ();
+		this.buildingListRectTransform = this.buildingList.GetComponent<RectTransform> ();
+		buildingButtonTransforms = new RectTransform[buildingListButtons.Length];
+		for (int i = 0; i < buildingListButtons.Length; i++) {
+			buildingButtonTransforms[i] = buildingListButtons[i].GetComponent<RectTransform>();
+		}
+
 		// Add unit List to prefab
 		this.unitList = (GameObject)Instantiate(unitListPrefab);
 		this.unitList.transform.SetParent(canvas.transform, false);
 		Button[] unitListButtons = unitList.GetComponentsInChildren<Button>();
+		this.unitListRectTransform = this.unitList.GetComponent<RectTransform> ();
+		unitButtonTransforms = new RectTransform[unitListButtons.Length];
+		for (int i = 0; i < unitListButtons.Length; i++) {
+			unitButtonTransforms[i] = unitListButtons[i].GetComponent<RectTransform>();
+		}
 		this.unitButton0 = unitListButtons [0];
 		this.unitButton1 = unitListButtons[1];
 	}
 	
 	private void Start() {
 
+		// Set initial sizes of GUI elements
+		this.canvasRectTransform = this.canvas.GetComponent<RectTransform> ();
+
+		this.miniMapRectTransform.sizeDelta = new Vector2(GUI_WIDTH,GUI_WIDTH);
+		this.miniMapRectTransform.anchoredPosition = new Vector3 (-1*GUI_WIDTH/2, -1*GUI_WIDTH/2);
+
+		this.tabTransform.sizeDelta = new Vector2 (GUI_WIDTH, TAB_HEIGHT);
+		this.tabTransform.anchoredPosition = new Vector3 (-1*GUI_WIDTH/2, -1*TAB_HEIGHT/2 - miniMapRectTransform.rect.height);
+
+		this.buildingTabTransform.sizeDelta = new Vector2 (GUI_WIDTH/2, TAB_HEIGHT*1.5f);
+		this.buildingTabTransform.anchoredPosition = new Vector2 (buildingTabTransform.sizeDelta.x/2, 0);
+
+		this.unitTabTransform.sizeDelta = new Vector2 (GUI_WIDTH/2, TAB_HEIGHT*1.5f);
+		this.unitTabTransform.anchoredPosition = new Vector2 (unitTabTransform.sizeDelta.x/2, 0);
+
+		this.buildingTabText.fontSize = 12;
+		this.unitTabText.fontSize = 12;
+
+		this.previousGuiButtonContainerHeight = canvasRectTransform.rect.height - GUI_WIDTH - TAB_HEIGHT;
+		this.guiButtonContainerHeight = canvasRectTransform.rect.height - GUI_WIDTH - TAB_HEIGHT;
+		this.buildingListRectTransform.sizeDelta = new Vector2 (GUI_WIDTH, guiButtonContainerHeight);
+		this.buildingListRectTransform.anchoredPosition = new Vector2 (-GUI_WIDTH/2, guiButtonContainerHeight/2);
+
+		this.unitListRectTransform.sizeDelta = new Vector2 (GUI_WIDTH, guiButtonContainerHeight);
+		this.unitListRectTransform.anchoredPosition = new Vector2 (-GUI_WIDTH/2, guiButtonContainerHeight/2);
+
+		float startX = 40;
+		float startY = -40;
+		float buttonSize = GUI_WIDTH / 2 - 10;
+		int buttonsPerRow = 2;
+
+		int index = 0;
+		int buildingRows = this.buildingButtonTransforms.Length / buttonsPerRow;
+		for (int i = 0; i < buildingRows; i++) {
+			for(int j = 0; j < buttonsPerRow; j++) {
+				this.buildingButtonTransforms[index].sizeDelta = new Vector2 (buttonSize, buttonSize);
+				this.buildingButtonTransforms[index].anchoredPosition = new Vector2 (startX+(j*(buttonSize+5)), startY+(-i*(buttonSize+5)));
+				index++;
+			}
+		}
+
+		index = 0;
+		int unitRows = this.unitButtonTransforms.Length / buttonsPerRow;
+		for (int i = 0; i < unitRows; i++) {
+			for(int j = 0; j < buttonsPerRow; j++) {
+				this.unitButtonTransforms[index].sizeDelta = new Vector2 (buttonSize, buttonSize);
+				this.unitButtonTransforms[index].anchoredPosition = new Vector2 (startX+(j*(buttonSize+5)), startY+(-i*(buttonSize+5)));
+				index++;
+			}
+		}
+	
 		// Set buildings tab as default selected tab
 		selectTab (buildingsTab);
 		unselectTab (unitsTab);
@@ -110,6 +195,19 @@ public class GUIManager : MonoBehaviour
 		// Give camera width of GUI
 		this.guiScreenWidth = tabTransform.rect.width;
 		this.cameraManager.setGUIWidths (this.guiScreenWidth);
+	}
+
+	private void Update() {
+
+		// Update GUI when screen size changes
+		this.guiButtonContainerHeight = this.canvasRectTransform.rect.height - GUI_WIDTH - TAB_HEIGHT;
+		if (this.guiButtonContainerHeight != this.previousGuiButtonContainerHeight) {
+			this.buildingListRectTransform.sizeDelta = new Vector2 (GUI_WIDTH, guiButtonContainerHeight);
+			this.buildingListRectTransform.anchoredPosition = new Vector2 (-GUI_WIDTH / 2, guiButtonContainerHeight / 2);
+
+			this.unitListRectTransform.sizeDelta = new Vector2 (GUI_WIDTH, guiButtonContainerHeight);
+			this.unitListRectTransform.anchoredPosition = new Vector2 (-GUI_WIDTH / 2, guiButtonContainerHeight / 2);
+		}
 	}
 
 	/*********************************************************************************/
